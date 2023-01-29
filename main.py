ccbcfe74c3904d0c8ac9b4f9c76d710d43218845
Taha Ahmed Abdelazim Hussein <71207531+TahaThinks@users.noreply.github.com>
2023-01-29 19:37:56 +0000

    Check if the Number is Odd or Even

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
 # Write your code below this line 👇
 print("Hello, Taha!")
+# Check if the number is odd or even
+string=input("Enter the number you wish to check:")
+
+number=int(string)
+
+if number%2==0:
+    print(f"The number {number} is even!")
+else:
+    print(f"The number {number} is odd!")
